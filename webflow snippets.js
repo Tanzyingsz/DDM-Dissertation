ca1a2cfe7d9f0ef4ee4a1726be5af2e7c204579a
Tanzyingsz <47805900+Tanzyingsz@users.noreply.github.com>
2020-07-11 21:41:33 +0100

    Create webflow snippets.js

---
+++ webflow snippets.js
@@ -0,0 +1,16 @@
+< !--smooth scrolling effect-- >
+    
+<script src="https://min30327.github.io/luxy.js/dist/js/luxy.js">
+</script>
+    
+    <script charset="utf-8">
+var isMobile = /iPhone|iPad|Android/i.test(navigator.userAgent);
+        if (!isMobile) {
+            luxy.init({
+                wrapper: '#luxy',
+                wrapperSpeed: 0.065,
+
+            });
+        }
+
+</script>
