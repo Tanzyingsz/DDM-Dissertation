b537d34ae0a90e43f3517868333f24bba2878638
Tanzyingsz <47805900+Tanzyingsz@users.noreply.github.com>
2020-07-24 23:19:32 +0100

    Create calculator.js

---
+++ calculator.js
@@ -0,0 +1,32 @@
+//Sofa
+var co2_sofa_2 = 135,
+  co2_sofa_3 = 180,
+  co2_sofa_4 = 225;
+var h2o_sofa_2 = 190.5,
+  h2o_sofa_3 = 254,
+  h2o_sofa_4 = 317.5;
+var landfill_sofa_2 = 129,
+  landfill_sofa_3 = 172,
+  landfill_sofa_4 = 215;
+
+//Bed
+var co2_single = 55,
+  co2_double = co2_single * 2,
+  co2_king = co2_single * 2.5;
+var h2o_single = 181843,
+  h2o_double = h2o_single * 2,
+  h2o_king = co2_single * 2.5;
+var landfill_single = 40.5,
+  landfill_double = landfill_single * 2,
+  landfill_king = co2_single * 2.5;
+
+//Storage
+var co2_shelf = 31,
+  co2_drawer = 48,
+  co2_bookcase = 18;
+var h2o_shelf = 218.2,
+  h2o_drawer = 150.7,
+  h2o_bookcase = 138.5;
+var landfill_shelf = 52,
+  landfill_drawer = 24,
+  landfill_bookcase = 33;
