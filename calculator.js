31ad70efece4e6b1bcea4afc1210d06eaad75487
Tanzyingsz <47805900+Tanzyingsz@users.noreply.github.com>
2020-07-25 08:52:17 +0100

    Update calculator.js

--- calculator.js
+++ calculator.js
@@ -2,9 +2,11 @@
 var co2_sofa_2 = 135,
   co2_sofa_3 = 180,
   co2_sofa_4 = 225;
+
 var h2o_sofa_2 = 190.5,
   h2o_sofa_3 = 254,
   h2o_sofa_4 = 317.5;
+
 var landfill_sofa_2 = 129,
   landfill_sofa_3 = 172,
   landfill_sofa_4 = 215;
@@ -13,9 +15,11 @@ var landfill_sofa_2 = 129,
 var co2_single = 55,
   co2_double = co2_single * 2,
   co2_king = co2_single * 2.5;
+
 var h2o_single = 181843,
   h2o_double = h2o_single * 2,
   h2o_king = co2_single * 2.5;
+
 var landfill_single = 40.5,
   landfill_double = landfill_single * 2,
   landfill_king = co2_single * 2.5;
@@ -24,9 +28,47 @@ var landfill_single = 40.5,
 var co2_shelf = 31,
   co2_drawer = 48,
   co2_bookcase = 18;
-var h2o_shelf = 218.2,
-  h2o_drawer = 150.7,
-  h2o_bookcase = 138.5;
+
+var h2o_shelf = 21821,
+  h2o_drawer = 150713,
+  h2o_bookcase = 138480;
+
 var landfill_shelf = 52,
   landfill_drawer = 24,
   landfill_bookcase = 33;
+
+//Lamp
+var co2_floorlamp = 114,
+  co2_tablelamp = 12;
+
+var h2o_floorlamp = 1437,
+  h2o_drawer = 1512;
+
+var landfill_floorlamp = 9.5,
+  landfill_drawer = 1;
+
+//Chair
+var co2_chair = 35,
+  co2_armchair = 43,
+  co2_officechair = 75;
+
+var h2o_chair = 36368,
+  h2o_armchair = 54552,
+  h2o_officechair = 90921;
+
+var landfill_chair = 4,
+  landfill_armchair = 6,
+  landfill_officechair = 10;
+
+//table
+var co2_desk = 48,
+  co2_bedside_table = 17,
+  co2_dressingtable = 75;
+
+var h2o_desk = 100013,
+  h2o_bedside_table = 50006,
+  h2o_dressingtable = 145474;
+
+var landfill_desk = 32,
+  landfill_bedside_table = 16,
+  landfill_dressingtable = 68;
